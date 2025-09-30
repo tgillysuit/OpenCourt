7e79ba4c2136547ade278f96e4740259af2ff9be
Rebecca Riffle <rebeccaeriffle@gmail.com>
2025-09-30 11:09:31 -0700

    Added server outline

---
+++ src/server/server.js
@@ -0,0 +1,11 @@
+// import express
+// documentation here: https://expressjs.com/
+
+// initialize express app
+
+// any middleware here
+// - use express.json()
+
+// use router || get, send on response object
+
+// app listen on port
