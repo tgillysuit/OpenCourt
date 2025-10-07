278080d790440fce8ef4e715c6064c3b1510374e
Rebecca Riffle <rebeccaeriffle@gmail.com>
2025-10-07 10:12:22 -0700

    Very basic form added

--- src/frontend/opencourt/src/App.jsx
+++ src/frontend/opencourt/src/App.jsx
@@ -2,9 +2,18 @@ import './App.css'
 
 function App() {
 
+  function addEvent({eventId}) {
+    console.log(eventId)
+  }
+
   return (
     <>
-      <h1>Open Court -- It Works!</h1>
+      <h1>Open Court</h1>
+      <h2>Add an Event</h2>
+      <form action={addEvent}>
+        <input type="text" name="eventId" value="Add Event Here" />
+        <button type="submit">Add Event</button>
+      </form>
     </>
   )
 }
