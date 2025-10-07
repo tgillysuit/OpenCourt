a12fcf832b2df10e1150c3dccc5a41ecdbbd25f1
Rebecca Riffle <rebeccaeriffle@gmail.com>
2025-10-07 11:31:42 -0700

    Moved db connection to separate db file

--- src/server/server.js
+++ src/server/server.js
@@ -1,17 +1,5 @@
 const express = require('express')
-const mysql = require('mysql2')
-require('dotenv').config()
-
-console.log(process.env.DB_USER)
-
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-})
-
-connection.connect()
+const connection = require('./db')
 
 const app = express()
 const port = 3000
