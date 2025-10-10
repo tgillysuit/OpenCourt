f82dd2982347488fcb3e732615bc141300bc6416
Rebecca Riffle <rebeccaeriffle@gmail.com>
2025-10-10 14:57:48 -0700

    Added fetch call to frontend

--- src/frontend/opencourt/src/App.jsx
+++ src/frontend/opencourt/src/App.jsx
@@ -1,18 +1,31 @@
 import './App.css'
+import { useState } from 'react'
 
 function App() {
 
-  function addEvent({eventId}) {
-    console.log(eventId)
+  // state
+  const [games, setGames] = useState();
+
+  // handle game button click
+  const onGamesClick = async () => {
+    try {
+      const result = await fetch('https://147.182.232.135:3000/games'); //TODO: .env variable here
+      const data = await result.json();
+      setGames(data);
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   return (
     <>
       <h1>Open Court</h1>
-      <h2>Add an Event</h2>
-      <form action={addEvent}>
-        <input type="text" name="eventId" value="Add Event Here" />
-        <button type="submit">Add Event</button>
+      <h2>Add a Game</h2>
+      <button onClick={onGamesClick} getGames>All Games</button>
+      <p>{games}</p>
+      <form>
+        <input type="text" name="gameId" value="Add Game Here" />
+        <button type="submit">Add Game</button>
       </form>
     </>
   )
--- src/server/routers/router.js
+++ src/server/routers/router.js
@@ -7,7 +7,19 @@ const controller = require('../controllers/controller')
 const router = express.Router();
 
 // CRUD
-// EVENT
+// Routes:
+/* 
+GET
+- all games
+- all users
+- all locations
+- all games_users
+POST
+- a game
+- a user
+- a location
+- a game_user
+*/
 router.get('/games', controller.getAllGames)
 router.post('/games', (req, res) => { res.send("ADDED EVENT")})
 
