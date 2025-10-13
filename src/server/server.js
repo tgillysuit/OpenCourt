2109d72fdc578d530468e5974ba67f5024131d61
Augy Markham <katiefaithmarkham@gmail.com>
2025-10-13 14:53:53 -0700

    Fixed env path?

--- src/server/server.js
+++ src/server/server.js
@@ -5,9 +5,9 @@ const cors = require('cors');
 const app = express()
 const port = 3000
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!!!!!')
-// })
+app.get('/', (req, res) => {
+  res.send('Hello World!!!!!')
+})
 
 app.use(cors());
 app.use("/", router)
