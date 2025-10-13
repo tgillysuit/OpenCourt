c8e11140be7e41b9d41ea4a3d7719037c7b97dcf
Augy Markham <katiefaithmarkham@gmail.com>
2025-10-13 13:24:06 -0700

    Made the JSON show up

--- src/frontend/opencourt/src/App.jsx
+++ src/frontend/opencourt/src/App.jsx
@@ -9,8 +9,9 @@ function App() {
   // handle game button click
   const onGamesClick = async () => {
     try {
-      const result = await fetch('https://146.190.62.102:3000/games'); //TODO: .env variable here
+      const result = await fetch('http://147.182.232.135:3000/games'); //TODO: .env variable here
       const data = await result.json();
+      
       setGames(data);
     } catch (error) {
       console.log(error)
@@ -21,8 +22,8 @@ function App() {
     <>
       <h1>Open Court</h1>
       <h2>Add a Game</h2>
-      <button onClick={onGamesClick} getGames>All Games</button>
-      <p>{games}</p>
+      <button onClick={onGamesClick}>All Games</button>
+      <pre>{JSON.stringify(games, null, 2)}</pre>
       <form>
         <input type="text" name="gameId" value="Add Game Here" />
         <button type="submit">Add Game</button>
