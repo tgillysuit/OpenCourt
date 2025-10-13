bfb8ea52bb3e89e2d48df652629e93b526377466
Rebecca Riffle <rebeccaeriffle@gmail.com>
2025-10-13 15:46:45 -0700

    turned db connection into db pool

--- src/server/model/db.js
+++ src/server/model/db.js
@@ -5,13 +5,11 @@ require('dotenv').config({ path: path.resolve(__dirname, '../../../.env') });
 
 console.log(process.env.DB_USER)
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
-}).promise()
-
-db.connect()
+}).promise();
 
 module.exports = db;
