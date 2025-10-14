d26bd5a4825640fb6d82bcbce568ca1634cb7068
Augy Markham <katiefaithmarkham@gmail.com>
2025-10-14 11:00:18 -0700

    Added form to test post games

--- src/frontend/opencourt/src/App.jsx
+++ src/frontend/opencourt/src/App.jsx
@@ -7,13 +7,44 @@ function App() {
 
   console.log(import.meta.env.VITE_FRONTEND_IP)
 
-  // state
-  const [games, setGames] = useState();
 
-  // handle game button click
+  const [games, setGames] = useState();
+  const [formData, setFormData] = useState({
+    game_name: "",
+    location_id: ""
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    
+    try {
+      const res = await fetch(`http://${import.meta.env.VITE_FRONTEND_IP}:3000/games`, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(formData)
+      })
+
+      const data = await res.json();
+      setFormData({game_name: "", location_id: ""});
+    } catch (err) {
+      console.error(err);
+    }
+
+    
+  }
+
+  // handle game button click // TODO: Do something with this
   const onGamesClick = async () => {
     try {
-      const result = await fetch(`http://${import.meta.env.VITE_FRONTEND_IP}:3000/games`); //TODO: .env variable here
+      const result = await fetch(`http://${import.meta.env.VITE_FRONTEND_IP}:3000/games`);
       const data = await result.json();
       
       setGames(data);
@@ -28,8 +59,28 @@ function App() {
       <h2>Add a Game</h2>
       <button onClick={onGamesClick}>All Games</button>
       <pre>{JSON.stringify(games, null, 2)}</pre>
-      <form>
-        <input type="text" name="gameId" value="Add Game Here" />
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Game Name:</label>
+          <input 
+            type="text" 
+            name= "game_name"
+            value={formData.game_name}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div>
+          <label>Location ID:</label>
+          <input 
+            type= "number" 
+            name= "location_id"
+            value={formData.location_id}
+            onChange={handleChange}
+          />
+        </div>
+        
         <button type="submit">Add Game</button>
       </form>
     </>
