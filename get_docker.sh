1a04fb32e0c7c4b875cb3801650c536aeb4c60ca
RMarx1456 <hellohi12465@gmail.com>
2025-10-29 21:37:08 -0700

    Added a get_docker.sh script for ease of installation on GUILESS Ubuntu.

---
+++ get_docker.sh
@@ -0,0 +1,20 @@
+sudo -v
+
+yes | sudo DEBIAN_FRONTEND=noninteractive apt-get -yqq upgrade
+
+sudo apt update
+
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt install docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce -y
